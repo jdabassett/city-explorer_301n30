322840f7d3e46ed4404c847f7b129a9ffec11c4a
jdabassett <jacobbassett@gmail.com>
2023-04-29 20:49:33 -0600

    small footer fix?

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import React from 'react'
-import Main from './components/main/Main.js'
-import Header from './components/header/Header.js'
-import Footer from './components/footer/Footer.js'
+import Main from './components/main/Main'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 
 
 export default class App extends React.Component {
